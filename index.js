d4f409c996b3a6875ae32815d49cc7b7e33f5319
Dave Kinkead <dave@kinkead.com.au>
2017-08-15 10:52:27 +1000

    Revert to passthrough mode

--- index.js
+++ index.js
@@ -4,29 +4,14 @@ const fs = require('fs')
 const rfs = require('require-from-string')
 const code_block = /^[ ]{4,}|\t+/
 
-function Literati () {}
+module.exports = (filename) => {
+  let code = fs.readFileSync(filename).toString().split('\n').filter((line) => {
+    //  TODO: Ignore YAML front matter
+    //  TODO: Deal with multi line lists
+    if (line.match(code_block)) {
+      return line
+    }
+  }).join('\n')
 
-Literati.prototype.read = (filename, opts) => {
-  return read(filename)
+  return rfs(code)
 }
-
-Literati.prototype.exec = (filename) => {
-  return exec(filename)()
-}
-
-function read (filename, opts) {
-    return fs.readFileSync(filename).toString().split('\n').filter((line) => {
-      //  TODO: Ignore YAML front matter
-      //  TODO: Deal with multi line lists
-      if (line.match(code_block)) {
-        return line
-      }
-    }).join('\n')
-  }
-
-function exec (filename, opts) {
-  return rfs( read(filename, opts) )
-}
-
-
-module.exports = new Literati()
